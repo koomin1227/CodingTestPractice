e45481c4a730b3bc64ef9c072ac033f370bc39e5
koomin <koomin1227@naver.com>
2022-08-09 20:04:05 +0900

    9935

---
+++ baekjoon/ETC/9935.py
@@ -0,0 +1,33 @@
+#https://www.acmicpc.net/problem/9935
+from sys import stdin
+from collections import deque
+from itertools import combinations
+import copy
+import heapq
+input=stdin.readline
+st=input().rstrip()
+boom=input().rstrip()
+boom_len=len(boom)
+boom_last=boom[-1]
+
+nst=[]
+cnt=0
+for i in range(len(st)):
+    nst.append(st[i])
+    if st[i]==boom_last and i>=boom_len-1 and len(nst)>=boom_len:
+        cnt=0
+        for j in range(boom_len):
+            if nst[-1-j]==boom[boom_len-j-1]:
+                cnt+=1
+        if cnt==boom_len:
+            for _ in range(boom_len):
+                nst.pop()
+
+if len(nst)==0:
+    print('FRULA')
+else:       
+    for i in range(len(nst)):
+        print(nst[i],end='')
+
+
+
--- transformation.py
+++ transformation.py
@@ -1,40 +1,10 @@
 from sys import stdin
 import copy
-n=int(stdin.readline())
-s=list(map(int,stdin.readline().split()))                                                                                                                                                  
-oper=list(map(int,stdin.readline().split()))
-mi=1000000001
-ma=-1000000001
-def dfs(x,oper,tot):
-    if oper[0]!=0:
-            oper[0]-=1
-            dfs(x+1,oper,tot+s[x+1])
-            oper[0]+=1
-    if oper[1]!=0:
-            oper[1]-=1
-            dfs(x+1,oper,tot-s[x+1])
-            oper[1]+=1
-    if oper[2]!=0:
-            oper[2]-=1
-            dfs(x+1,oper,tot*s[x+1])
-            oper[2]+=1
-    if oper[3]!=0:
-            oper[3]-=1
-            if tot>0:
-                dfs(x+1,oper,tot//s[x+1])
-            else:
-                dfs(x+1,oper,((tot*-1)//s[x+1])*-1)
-            oper[3]+=1
-    if (x==n-1):
-        global mi
-        global ma
-        if mi>tot:
-            mi=tot
-        if ma<tot:
-            ma=tot   
-dfs(0,oper,s[0])
-print(ma)
-print(mi)
+for i in range(10):
+    if i==2:
+        i=5
+    print(i)
+
 
 
 
