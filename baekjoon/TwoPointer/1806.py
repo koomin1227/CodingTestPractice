3b77b3004d721a0dfb3651bb85f4b40555ca3ca1
koomin <koomin1227@naver.com>
2023-01-10 18:09:45 +0900

    1806v.2

--- baekjoon/TwoPointer/1806.py
+++ baekjoon/TwoPointer/1806.py
@@ -4,29 +4,39 @@ input=stdin.readline
 
 n,s=map(int,input().split())
 pro=list(map(int,input().split()))
-l=n-1
-f=0
-r=0
+f,r=0,0
 min_len=n+1
 tot=pro[0]
-while r<n:
-    if f<n-1:
-        if tot>=s:
-            min_len=min(min_len,f-r+1)
-            if f==r:
-                f+=1
-                tot+=pro[f]
-            else:
-                tot-=pro[r]
-                r+=1
-        elif tot<s:
-            f+=1
-            tot+=pro[f]  
-    else:
-        if tot>=s:
-            min_len=min(min_len,f-r+1)
+#간략한 버전
+while True:
+    if tot>=s:
         tot-=pro[r]
+        min_len=min(min_len,f-r+1)
         r+=1
+    else:
+        f+=1
+        if f==n:
+            break
+        tot+=pro[f]
+#혼자 짜본 투포인터 버전
+# while r<n:
+#     if f<n-1:
+#         if tot>=s:
+#             min_len=min(min_len,f-r+1)
+#             if f==r:
+#                 f+=1
+#                 tot+=pro[f]
+#             else:
+#                 tot-=pro[r]
+#                 r+=1
+#         elif tot<s:
+#             f+=1
+#             tot+=pro[f]  
+#     else:
+#         if tot>=s:
+#             min_len=min(min_len,f-r+1)
+#         tot-=pro[r]
+#         r+=1
 if min_len==n+1:
     print(0)
 else:
