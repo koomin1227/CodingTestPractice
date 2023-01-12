eaa52a502881ddd00f93f88e8fbaaa8372c4472f
koomin <koomin1227@naver.com>
2023-01-12 22:04:40 +0900

    2473.ver.2.0

--- baekjoon/TwoPointer/2473.py
+++ baekjoon/TwoPointer/2473.py
@@ -14,35 +14,19 @@ def minimum(mini,a,f,r,sum1):
     return mini
 #투포인터
 
-
 ans=[3000000000,0,0,0]
-for i in range(n):
-    r=0
+for i in range(n-2):
+    r=i+1
     f=n-1
-    if r==i:
-        r+=1
-    elif f==i:
-        f-=1
-    mini=[3000000000,f,r,0]
-    target=0-arr[i]
-    while True:
-        cur_sum=arr[r]+arr[f]
-        cur_sub=abs(cur_sum-target)
-        mini=minimum(mini,cur_sub,f,r,cur_sum)
-        if cur_sum<target:
+    while r<f:
+        cur_sum=arr[r]+arr[f]+arr[i]
+        ans=minimum(ans,abs(cur_sum),f,r,i)
+        if cur_sum<0:
             r=r+1
-            if r==i:
-                r+=1
         else:
             f=f-1
-            if f==i:
-                f-=1
-        if r==f:
-            break
-    ans=minimum(ans,abs(mini[3]+arr[i]),mini[2],mini[1],i)
 
 res=[arr[ans[1]], arr[ans[2]],arr[ans[3]]]
 res.sort()    
 for i in res:
     print(i,end=' ')
-# print(ans)
