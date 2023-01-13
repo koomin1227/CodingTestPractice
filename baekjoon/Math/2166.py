dd6f0a4f3d71d9a61790f5bddf1c998c129c6dca
koomin <koomin1227@naver.com>
2023-01-13 17:51:55 +0900

    test

--- baekjoon/Math/2166.py
+++ baekjoon/Math/2166.py
@@ -1,17 +1,13 @@
 #https://www.acmicpc.net/problem/2166
 from sys import stdin
 input=stdin.readline
-
 n=int(input())
-coordinate=[]
+cod=[]
 for i in range(n):
-    coordinate.append(list(map(int,input().split())))
-coordinate.append(coordinate[0])
-xSum=0
-ySum=0
+    cod.append(list(map(int,input().split())))
+cod.append(cod[0])
+xSum,ySum=0,0
+area=0
 for i in range(n):
-    xSum+=coordinate[i][0]*coordinate[i+1][1]
-    ySum+=coordinate[i][1]*coordinate[i+1][0]
-area=abs(xSum-ySum)/2
-
-print(round(abs(xSum-ySum)/2,1))
+    area+=cod[i][0]*cod[i+1][1]-cod[i][1]*cod[i+1][0]
+print(round(abs(area)/2,1))
--- bronze.py
+++ bronze.py
@@ -3,4 +3,5 @@ b=int(input())
 print(a+b)
 print(a-b)
 print(a*b)
+print(a/b)
 
