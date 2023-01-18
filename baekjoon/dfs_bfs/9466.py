33af04187735fb730219dc1131d27801889d94ef
koomin <koomin1227@naver.com>
2023-01-18 19:37:30 +0900

    9466

--- baekjoon/dfs_bfs/9466.py
+++ baekjoon/dfs_bfs/9466.py
@@ -2,39 +2,30 @@
 import sys
 sys.setrecursionlimit(100001)
 input=sys.stdin.readline
-def dfs(s,t):
-    global tot
+def dfs(s):
+    global tot,ans
+    isVisit[s]=True
+    cycle.append(s)
     now=graph[s]
-    if isVisit[now]==False or now==t:
-        isVisit[now]=True
-        tot+=1
-        if now==t:
-            return True
-        if dfs(now,t)==False:
-            isVisit[now]=False 
-            tot-=1
-            return False
-        else:
-            return True
-    return False
+    if isVisit[now]==True:
+        if now in cycle:
+            ans=ans+cycle[cycle.index(now):]
+        return
+    else:
+        dfs(now)
+
+    
 T=int(input())
 for t in range(T):
-    ans=0
+    ans=[]
     n=int(input())
     graph=[0]
     graph=graph+list(map(int,input().split()))
     isVisit=[False]*(n+1)
-    tot=0
     for i in range(1,n+1):
+        cycle=[]
         if isVisit[i]==False:
-            isVisit[i]=True
-            tot+=1
-            if dfs(i,i)==False:
-                isVisit[i]=False
-            tot-=1
-    # for i in range(1,n+1):
-    #     if isVisit[i]==False:
-    #         ans+=1
-    print(n-tot)
+            dfs(i)
+    print(n-len(ans))
 
     
