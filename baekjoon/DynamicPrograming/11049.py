97c17f64b3563c000013b5402c518213d5a1286c
koomin <koomin1227@naver.com>
2023-01-24 18:15:20 +0900

    11049

--- baekjoon/DynamicPrograming/11049.py
+++ baekjoon/DynamicPrograming/11049.py
@@ -4,11 +4,18 @@ input=stdin.readline
 
 n=int(input())
 ma=[]
-dp=[0]*n
+dp=[[0]*n for _ in range(n)]
 
 for i in range(n):
     ma.append(list(map(int,input().split())))
-dp[1]=ma[0][0]*ma[0][1]*ma[1][1]
-for i in range(2,n):
-    dp[i]=min(dp[i-1]+(ma[0][0]*ma[i][0]*ma[i][1]), dp[i-2]+(ma[i-1][0]*ma[i-1][1]*ma[i][1])+(ma[0][0]*ma[i-1][0]*ma[i][1]))
-print(dp[n-1])
+for i in range(1,n):
+    for j in range(0,n-i):
+        r,c=j,i+j
+        dp[r][c]=2147483648
+        for k in range(r,c):
+            dp[r][c]=min(dp[r][c],dp[r][k]+dp[k+1][c]+(ma[r][0]*ma[k][1]*ma[c][1]))
+for i in dp:
+    print(i)
+
+print(dp[0][n-1])
+
