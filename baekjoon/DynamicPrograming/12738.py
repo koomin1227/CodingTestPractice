a4653b54bf433d2fd1a246977a52f24aaea58851
koomin <koomin1227@naver.com>
2023-01-26 21:42:11 +0900

    12015,12738

---
+++ baekjoon/DynamicPrograming/12738.py
@@ -0,0 +1,28 @@
+#https://www.acmicpc.net/problem/12738
+from sys import stdin
+input=stdin.readline
+
+def binary(l,r,k):
+    while(l<r):
+        m=(l+r)//2
+        if se[m]<k:
+            l=m+1
+        elif se[m]>=k:
+            r=m
+    else:
+        return r
+n=int(input())
+series=list(map(int,input().split()))
+se=[0,series[0]]
+
+
+leng=1
+for i in range(1,n):
+    if se[leng]<series[i]:
+        leng+=1
+        se.append(series[i])
+        continue
+    tmp=binary(1,leng,series[i])
+    se[tmp]=series[i]
+
+print(leng)
--- baekjoon/DynamicPrograming/9252.py
+++ baekjoon/DynamicPrograming/9252.py
@@ -7,24 +7,25 @@ str1=input()
 str2=input()
 len1=len(str1)-1
 len2=len(str2)-1
-dp=[[0]*(len2+1)for _ in range(len1+1)]
+dp=[[0]*(len1+1)for _ in range(len2+1)]
 ans=deque([])
-for i in range(1,len1+1):
-    for j in range(1,len2+1):
-        if str1[i-1]==str2[j-1]:
+for i in range(1,len2+1):
+    for j in range(1,len1+1):
+        if str2[i-1]==str1[j-1]:
             dp[i][j]=dp[i-1][j-1]+1
         dp[i][j]=max(dp[i][j],dp[i-1][j],dp[i][j-1])
-i,j=len1,len2
+i,j=len2,len1
 while True:
-    
-    if dp[i-1][j]==dp[i][j]:
-        i-=1
-    elif dp[i-1][j]<dp[i][j]:
-        j-=1
-        ans.appendleft(str1[i-1])
-        if dp[i-1][j]==0:
+    if dp[i][j]==0:
             break
-print(dp[len1][len2])
+    if dp[i][j-1]==dp[i][j]:
+        j-=1
+    elif dp[i][j-1]<dp[i][j]:
+        if dp[i-1][j]<dp[i][j]:
+            ans.appendleft(str1[j-1])
+        i-=1
+        
+print(dp[len2][len1])
 for i in ans:
     print(i,end='')
 #print(str(ans.reverse))
