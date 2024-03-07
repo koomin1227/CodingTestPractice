024f7b1803981b89507a9ac7944d9bafae4d081c
koomin1227 <koomin1227@naver.com>
2024-03-07 17:18:37 +0900

    없어진 기록 찾기

---
+++ programmers/SQL/level2/없어진_기록_찾기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT o.ANIMAL_ID, o.NAME from ANIMAL_INS i
+    right join ANIMAL_OUTS o
+    on i.ANIMAL_ID = o.ANIMAL_ID
+    where i.ANIMAL_ID is NULL
+    order by i.ANIMAL_ID;
+    
+    
+    
