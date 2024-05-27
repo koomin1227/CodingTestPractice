133748283c130d1dcd124fe9e4ef2296d6e75091
koomin1227 <koomin1227@naver.com>
2024-05-27 15:04:54 +0900

    21924

---
+++ baekjoon/MST/21924.py
@@ -0,0 +1,44 @@
+from sys import stdin
+input = stdin.readline
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n,m=map(int,input().split())
+parent = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    parent[i] = i
+
+edges = []
+result = 0
+total_cost = 0
+
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    total_cost += cost
+    edges.append((cost, a, b))
+
+edges.sort()
+tot=[]
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        tot.append(cost)
+
+if len(tot) == n - 1:
+    print(total_cost - result)
+else:
+    print(-1)
