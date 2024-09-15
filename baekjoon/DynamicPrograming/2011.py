df3e4ea802af717d417fd89f252b1c7189d53a70
Koo Min <koomin1227@naver.com>
2024-09-15 21:34:49 +0900

    2011

--- baekjoon/DynamicPrograming/2011.py
+++ baekjoon/DynamicPrograming/2011.py
@@ -1,7 +1,7 @@
 from sys import stdin
 input = stdin.readline
 n = input()
-nums = [int(i) for i in list(n)[:-2]]
+nums = [int(i) for i in list(n)[:-1]]
 dp = [1] + [0] * (len(nums))
 dp[1] = 1
 is_decode = True
@@ -9,7 +9,7 @@ if nums[0] == 0:
     is_decode = False
 for i in range(2, len(dp)):
     if nums[i - 1] > 0:
-        dp[i] = dp[i-1]
+        dp[i] += dp[i-1]
     if nums[i-2] * 10 + nums[i - 1] <= 26 and nums[i-2] * 10 + nums[i - 1] >= 10:
         dp[i] += dp[i-2]
 
